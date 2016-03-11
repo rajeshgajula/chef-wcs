57e956e070b45f640f94526a42d1dff507fdff05
billMeLater <vadim.kalitsev@tailoredbrands.com>
2016-03-11 06:25:43 -0500

    fp version to 39

--- wcsattr/attributes/default.rb
+++ wcsattr/attributes/default.rb
@@ -1,27 +1,27 @@
 
-default['updi']['wcs']['version']	=	'7008'
+default['updi']['wcs']['version']	=	'70091'
 default['updi']['wcs']['home']		=	'/usr/opt/app/IBM/updi_wcs'
 default['updi']['wcs']['user']		=	'wcadmin'
 default['updi']['wcs']['group']         =       'wcdvlp'
 
-default['updi']['was']['version']	=	'7.0.0.37'
+default['updi']['was']['version']	=	'7.0.0.39'
 default['updi']['was']['home']		=	'/usr/opt/app/IBM/updi_was'
 default['updi']['was']['user']		=	'wcadmin'
 default['updi']['was']['group']         =       'wcdvlp'
 
 default['repository']			=	'/usr/opt/app/IBM/media'
 
-default['was']['fp_version']            =       '7.0.0.37'
+default['was']['fp_version']            =       '7.0.0.39'
 default['was']['home']                  =       '/usr/opt/app/IBM/WebSphere/AppServer'
 default['was']['user']                  =       'wcadmin'
 default['was']['group']                 =       'wcdvlp'
 
-default['ihs']['fp_version']            =       '7.0.0.37'
+default['ihs']['fp_version']            =       '7.0.0.39'
 default['ihs']['home']                  =       '/usr/opt/app/IBM/HTTPServer'
 default['ihs']['user']                  =       'wcadmin'
 default['ihs']['group']                 =       'wcdvlp'
 
-default['plg']['fp_version']            =       '7.0.0.37'
+default['plg']['fp_version']            =       '7.0.0.39'
 default['plg']['home']                  =       '/usr/opt/app/IBM/HTTPServer/Plugins'
 default['plg']['user']                  =       'wcadmin'
 default['plg']['group']                 =       'wcdvlp'
