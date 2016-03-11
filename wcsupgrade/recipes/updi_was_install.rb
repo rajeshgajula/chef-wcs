d3a0d92d1a2ef0165726c29425b36b9d53704575
billMeLater <vadim.kalitsev@tailoredbrands.com>
2016-03-11 10:38:07 -0500

    added download updi distr from IBM ftp

--- wcsupgrade/recipes/updi_was_install.rb
+++ wcsupgrade/recipes/updi_was_install.rb
@@ -4,6 +4,14 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+remote_file "#{node['repository']}/#{node['updi']['was']['version']}-WS-UPDI-LinuxAMD64.zip" do
+        source  "ftp://public.dhe.ibm.com/software/websphere/appserv/support/tools/UpdateInstaller/7.0.x/LinuxAMD64/#{node['updi']['was']['version']}-WS-UPDI-LinuxAMD64.zip"
+        owner   node['updi']['was']['user']
+        group   node['updi']['was']['group']
+        mode    '0444'
+        action  :create_if_missing
+end
+
 wcsupgrade_updi 'updi-was' do
         productHome	node['updi']['was']['home']
 	productUser	node['updi']['was']['user']
--- wcsupgrade/recipes/updi_wcs_install.rb
+++ wcsupgrade/recipes/updi_wcs_install.rb
@@ -4,6 +4,15 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+
+remote_file "#{node['repository']}/download.updii.#{node['updi']['wcs']['version']}.linux.amd64.zip" do
+        source  "ftp://public.dhe.ibm.com/software/websphere/commerce/70/updi#{node['updi']['wcs']['version']}/download.updii.#{node['updi']['wcs']['version']}.linux.amd64.zip"
+        owner   node['updi']['wcs']['user']
+        group   node['updi']['wcs']['group']
+        mode    '0444'
+        action  :create_if_missing
+end
+
 wcsupgrade_updi 'updi-wcs' do
 	productHome	node['updi']['wcs']['home']
 	productUser	node['updi']['wcs']['user']
