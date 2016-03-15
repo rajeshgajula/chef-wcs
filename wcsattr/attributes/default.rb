a71c04e35c55090933e57f21ea9209260465b17a
billMeLater <vadim.kalitsev@tailoredbrands.com>
2016-03-15 10:02:16 -0400

    path changed for updi

--- wcsattr/attributes/default.rb
+++ wcsattr/attributes/default.rb
@@ -1,35 +1,31 @@
 
 default['updi']['wcs']['version']	=	'70091'
-default['updi']['wcs']['home']		=	'/usr/opt/app/IBM/updi_wcs'
+default['updi']['wcs']['home']		=	'/usr/opt/app/IBM/WebSphere/wcs_updi'
 default['updi']['wcs']['user']		=	'wcadmin'
-default['updi']['wcs']['group']         =       'wcdvlp'
+default['updi']['wcs']['group']         =       'wctest'
 
 default['updi']['was']['version']	=	'7.0.0.39'
-default['updi']['was']['home']		=	'/usr/opt/app/IBM/updi_was'
+default['updi']['was']['home']		=	'/usr/opt/app/IBM/WebSphere/was_updi'
 default['updi']['was']['user']		=	'wcadmin'
-default['updi']['was']['group']         =       'wcdvlp'
+default['updi']['was']['group']         =       'wctest'
 
 default['repository']			=	'/usr/opt/app/IBM/media'
 
 default['was']['fp_version']            =       '7.0.0.39'
 default['was']['home']                  =       '/usr/opt/app/IBM/WebSphere/AppServer'
 default['was']['user']                  =       'wcadmin'
-default['was']['group']                 =       'wcdvlp'
+default['was']['group']                 =       'wctest'
 
 default['ihs']['fp_version']            =       '7.0.0.39'
 default['ihs']['home']                  =       '/usr/opt/app/IBM/HTTPServer'
 default['ihs']['user']                  =       'wcadmin'
-default['ihs']['group']                 =       'wcdvlp'
+default['ihs']['group']                 =       'wctest'
 
 default['plg']['fp_version']            =       '7.0.0.39'
 default['plg']['home']                  =       '/usr/opt/app/IBM/HTTPServer/Plugins'
 default['plg']['user']                  =       'wcadmin'
-default['plg']['group']                 =       'wcdvlp'
+default['plg']['group']                 =       'wctest'
 
 
-default['wcupgrade']['user'] = 'wcadmin'
-default['wcupgrade']['group'] = 'wctest'
 
 default['wcupgrade']['wc_home'] = '/usr/opt/app/IBM/WebSphere/CommerceServer'
-default['wcupgrade']['was_home'] = '/usr/opt/app/IBM/WebSphere/AppServer'
-default['wcupgrade']['ihs_home'] = '/usr/opt/app/HTTPServer'
